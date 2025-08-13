9d1c4e1f46612fcb78e985e604425d6d86ed74d5
leejaemanman2 <dlwoaks97@naver.com>
2025-08-13 09:17:07 +0900

    8월13일 실습

---
+++ front/javascript/05-15-main.js
@@ -0,0 +1,17 @@
+import { plus, minus, height, weight, Animal }
+from './05-15-module.js';
+
+console.log(plus());
+console.log(minus());
+
+console.log(height);
+console.log(weight);
+
+const animal = new Animal('ShuShu');
+console.log(animal.name);
+
+const color = new color('red');
+
+console.log(color);
+
+
---
+++ front/javascript/05-15-module.js
@@ -0,0 +1,25 @@
+export function plus() {
+    return 2+1;
+}
+
+export function minus() {
+    return 2-1;
+}
+
+export const height = 178.4;
+
+export let weight = 74.3;
+weight = 86; // 값 변경
+
+export class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+
+}
+
+export class color {
+    constructor(color) {
+        this.color = color;
+    }
+}
